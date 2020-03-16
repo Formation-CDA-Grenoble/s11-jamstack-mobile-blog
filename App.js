8f19fc1427808e3d7ad2615a297ddfe0ce77688c
Romain Tello <romain.tello@gmail.com>
2020-03-16 13:54:58 +0100

    Fetch articles titles from DatoCMS

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const query = `
+query MyQuery {
+  allArticles {
+    title
+    createdAt
+    content
+    cover {
+      url
+    }
+    category {
+      name
+    }
+  }
 }
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends Component {
+  state = {
+    data: null,
+  }
+
+  componentDidMount = () => {
+    Axios.post(
+      'https://graphql.datocms.com/',
+      { query },
+      {
+        headers: {
+          Authorization: 'Bearer 6138772c24e2df7d8971d1adeed11e',
+        },
+      }
+    )
+    .then( response => this.setState({ data: response.data.data }))
+    .catch( error => { throw error; } );
+  }
+
+  render = () => {
+    const { data } = this.state;
+
+    if (data === null) {
+      return (
+        <View>
+          <Text>
+            Loading...
+          </Text>
+        </View>
+      );
+    }
+
+    return (
+      <View>
+        {data.allArticles.map( (article, index) =>
+          <Text key={index}>{article.title}</Text>
+        )}
+      </View>
+    );
+  }
+}
